944094ec1366da9aa86e0d1b36d625b06fc74826
veta41 <74262484+veta41@users.noreply.github.com>
2022-09-25 18:02:02 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,61 +5,63 @@ import { nanoid } from "nanoid";
 import ContactForm from "./ContactForm/ContactForm";
 import Filter from "./Filter/Filter";
 import ContactList from "./Contacts/ContactList";
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact, deleteContact, filterContact } from 'redux/action';
 
 
 function App(){
 
-  const [contacts, setContacts] = useState(() => 
-  JSON.parse(localStorage.getItem('contacts')) ?? 
+  // const [contacts, setContacts] = useState(() => 
+  // JSON.parse(localStorage.getItem('contacts')) ?? 
   
-  [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]
-  );
+  // [
+  // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ]
+  // );
 
-  const [filter, setFilter] = useState('');
+  // const [filter, setFilter] = useState('');
 
-   useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts))
-   }, [contacts])
+  //  useEffect(() => {
+  //   window.localStorage.setItem('contacts', JSON.stringify(contacts))
+  //  }, [contacts])
 
-
-  
-  const onDeleteButton = idContact => {
-    setContacts(contacts.filter(contact => contact.id !== idContact));
-      };
 
   
+   const onDeleteButton = idContact => {
+     setContacts(contacts.filter(contact => contact.id !== idContact));
+       };
 
-      const addContact = ({ name, number }) => {
-        const id = nanoid();
-        const contactItem = {
-          id,
-          name,
-          number,
-        };
+  
+
+  //     const addContact = ({ name, number }) => {
+  //       const id = nanoid();
+  //       const contactItem = {
+  //         id,
+  //         name,
+  //         number,
+  //       };
     
-        if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
-          alert(`${name} is already in contacts`);
-          return;
-        };
+  //       if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+  //         alert(`${name} is already in contacts`);
+  //         return;
+  //       };
     
-        setContacts(prevState => [contactItem, ...prevState]
-        );
-      };
+  //       setContacts(prevState => [contactItem, ...prevState]
+  //       );
+  //     };
     
       const changeFilter = event => {
-        setFilter(event.currentTarget.value);    
-       };
+       setFilter(event.currentTarget.value);    
+        };
     
       
       const normalizedFilter = filter.toLocaleLowerCase();
-      const filteredContactList = contacts.filter(contact =>
+       const filteredContactList = contacts.filter(contact =>
         contact.name.toLowerCase().includes(normalizedFilter),
-      );
+       );
 
     return (
       <Box
